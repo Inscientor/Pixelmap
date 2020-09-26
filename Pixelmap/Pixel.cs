736549868a688728d0da6bea4076b9ead6d5b957
Inscientor <scriptum_sacla@outlook.com>
2020-09-26 22:28:29 +0900

    作成

---
+++ Pixelmap/Pixel.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixelmap
+{
+    public class Pixel
+    {
+        #region プロパティ
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+        public byte A { get; set; }
+        #endregion
+
+        #region コンストラクタ
+        /// <summary>
+        /// 不透明な黒
+        /// </summary>
+        public Pixel()
+        {
+            R = 0x00;
+            G = 0x00;
+            B = 0x00;
+            A = 0xFF;
+        }
+        /// <summary>
+        /// 色を256段階で指定
+        /// </summary>
+        /// <param name="a">不透明度</param>
+        /// <param name="r">赤</param>
+        /// <param name="g">緑</param>
+        /// <param name="b">青</param>
+        public Pixel(byte a, byte r, byte g, byte b) => FromARGB(a, r, g, b);
+
+        /// <summary>
+        /// 色を256段階で指定
+        /// </summary>
+        /// <param name="r">赤</param>
+        /// <param name="g">緑</param>
+        /// <param name="b">青</param>
+        public Pixel(byte r, byte g, byte b) => FromARGB(r, g, b);
+
+        /// <summary>
+        /// Color構造体で指定
+        /// </summary>
+        /// <param name="color">色</param>
+        public Pixel(Color color)
+        {
+            FromColor(color);
+        }
+
+        /// <summary>
+        /// (B,G,R,A)の順として解釈
+        /// 足りなければ初期値(色なら0x00、不透明度なら0xFF) 余分は無視
+        /// </summary>
+        /// <param name="bytes">(B,G,R,A)</param>
+        public Pixel(IEnumerable<byte> bytes) => FromBytes(bytes);
+        #endregion
+
+        #region From系メソッド
+        /// <summary>
+        /// 色を256段階で指定
+        /// </summary>
+        /// <param name="a">不透明度</param>
+        /// <param name="r">赤</param>
+        /// <param name="g">緑</param>
+        /// <param name="b">青</param>
+        public void FromARGB(byte a, byte r, byte g, byte b)
+        {
+            A = a;
+            R = r;
+            G = g;
+            B = b;
+        }
+
+        /// <summary>
+        /// 色を256段階で指定
+        /// </summary>
+        /// <param name="r">赤</param>
+        /// <param name="g">緑</param>
+        /// <param name="b">青</param>
+        public void FromARGB(byte r, byte g, byte b) => FromARGB(0xFF, r, g, b);
+
+        /// <summary>
+        /// Color構造体で指定
+        /// </summary>
+        /// <param name="color">色</param>
+        public void FromColor(Color color)
+        {
+            R = color.R;
+            G = color.G;
+            B = color.B;
+            A = color.A;
+        }
+
+        /// <summary>
+        /// (B,G,R,A)の順として解釈
+        /// 足りなければ初期値(色なら0x00、不透明度なら0xFF) 余分は無視
+        /// </summary>
+        /// <param name="bytes">(B,G,R,A)</param>
+        public void FromBytes(IEnumerable<byte> bytes)
+        {
+            B = bytes.ElementAtOrDefault(0);
+            G = bytes.ElementAtOrDefault(1);
+            R = bytes.ElementAtOrDefault(2);
+            try
+            {
+                A = bytes.ElementAt(3);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                A = 0xFF;
+            }
+        }
+        #endregion
+
+        #region To系メソッド
+        /// <summary>
+        /// Color構造体に変換
+        /// </summary>
+        /// <returns>Color</returns>
+        public Color ToColor() => Color.FromArgb(A, R, G, B);
+        /// <summary>
+        /// (B,G,R,A)の順で格納したIEnumerable&lt;byte&gt;に変換
+        /// </summary>
+        /// <returns>Bytes</returns>
+        public IEnumerable<byte> ToBytes() => new byte[] { B, G, R, A };
+        #endregion
+    }
+}
---
+++ Pixelmap/Pixelmap.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+
+namespace Pixelmap
+{
+    /// <summary>
+    /// Bitmapをピクセル単位で高速に扱える
+    /// 常にFormat32bppArgbに変換する
+    /// Dispose()時にコンストラクタで設定したBitmapに変更を反映する
+    /// 
+    /// 注意:
+    /// Bitmapの開放は行わない
+    /// </summary>
+    public class Pixelmap : IDisposable, IEnumerable<Pixel>
+    {
+        Pixel[] pixels;
+        BitmapData data;
+        private Bitmap bitmap;
+
+        /// <summary>
+        /// ピクセルの形式
+        /// </summary>
+        public PixelFormat PixelFormat { get; private set; }
+        /// <summary>
+        /// 幅
+        /// </summary>
+        public int Width { get; }
+        /// <summary>
+        /// 高
+        /// </summary>
+        public int Height { get; }
+        /// <summary>
+        /// ピクセル数
+        /// </summary>
+        public int Count => pixels.Length;
+        /// <summary>
+        /// byte長
+        /// </summary>
+        public int Length => Math.Abs(data.Stride) * data.Height;
+
+        public Bitmap Bitmap
+        {
+            get
+            {
+                Commit();
+                return bitmap;
+            }
+
+            private set => bitmap = value;
+        }
+
+        private void Commit()
+        {
+            System.Runtime.InteropServices.Marshal.Copy(pixels.SelectMany(p => p.ToBytes()).ToArray(), 0, data.Scan0, pixels.Length * 4);
+            bitmap.UnlockBits(data);
+
+            data = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format32bppArgb);
+        }
+
+        /// <summary>
+        /// 2次元位置指定
+        /// </summary>
+        /// <param name="x">横座標</param>
+        /// <param name="y">縦座標</param>
+        /// <returns>指定ピクセル</returns>
+        public Pixel this[int x, int y]
+        {
+            get
+            {
+                return pixels[x + y * Width];
+            }
+            set
+            {
+                pixels[x + y * Width] = value;
+            }
+        }
+
+        /// <summary>
+        /// 1次元位置指定
+        /// </summary>
+        /// <param name="i">順番号</param>
+        /// <returns>指定ピクセル</returns>
+        public Pixel this[int i]
+        {
+            get
+            {
+                return pixels[i];
+            }
+            set
+            {
+                pixels[i] = value;
+            }
+        }
+
+        /// <summary>
+        /// PixelMapの元になるBitmapを指定
+        /// BitmapのDisposeは呼び出し側ですること
+        /// </summary>
+        /// <param name="bitmap">元となるBitmap</param>
+        public Pixelmap(Bitmap bitmap)
+        {
+            PixelFormat = bitmap.PixelFormat;
+
+            Width = bitmap.Width;
+            Height = bitmap.Height;
+            this.Bitmap = bitmap;
+            data = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadWrite,
+                PixelFormat);
+
+            pixels = MakePixels();
+        }
+
+        private Pixel[] MakePixels()
+        {
+            IEnumerable<Pixel> pixels = PixelFormat switch
+            {
+                PixelFormat.Indexed => throw new NotImplementedException(),
+                PixelFormat.Gdi => throw new NotImplementedException(),
+                PixelFormat.Alpha => throw new NotImplementedException(),
+                PixelFormat.PAlpha => throw new NotImplementedException(),
+                PixelFormat.Extended => throw new NotImplementedException(),
+                PixelFormat.Canonical => throw new NotImplementedException(),
+                PixelFormat.Undefined => throw new NotImplementedException(),
+                PixelFormat.Format1bppIndexed => throw new NotImplementedException(),
+                PixelFormat.Format4bppIndexed => throw new NotImplementedException(),
+                PixelFormat.Format8bppIndexed => MakePixelsFrom8bppIndexed(),
+                PixelFormat.Format16bppGrayScale => throw new NotImplementedException(),
+                PixelFormat.Format16bppRgb555 => throw new NotImplementedException(),
+                PixelFormat.Format16bppRgb565 => throw new NotImplementedException(),
+                PixelFormat.Format16bppArgb1555 => throw new NotImplementedException(),
+                PixelFormat.Format24bppRgb => throw new NotImplementedException(),
+                PixelFormat.Format32bppRgb => throw new NotImplementedException(),
+                PixelFormat.Format32bppArgb => MakePixelsFrom32bppArgb(),
+                PixelFormat.Format32bppPArgb => throw new NotImplementedException(),
+                PixelFormat.Format48bppRgb => throw new NotImplementedException(),
+                PixelFormat.Format64bppArgb => throw new NotImplementedException(),
+                PixelFormat.Format64bppPArgb => throw new NotImplementedException(),
+                PixelFormat.Max => throw new NotImplementedException(),
+                _ => throw new InvalidEnumArgumentException("PixelFormatが無効です。")
+            };
+
+            // dataの中身が変わる場合があるので,ここで評価して値を確定させる
+            Pixel[] pixelArray = pixels.ToArray();
+
+            // PixelFormatが32bppArgb以外の時はバイト列の大きさが変わるので領域を確保し直す
+            if (PixelFormat != PixelFormat.Format32bppArgb)
+            {
+                bitmap.UnlockBits(data);
+
+                Bitmap = new Bitmap(Width, Height, PixelFormat.Format32bppArgb);
+                data = bitmap.LockBits(
+                    new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                    ImageLockMode.ReadWrite,
+                    PixelFormat.Format32bppArgb);
+
+                PixelFormat = PixelFormat.Format32bppArgb;
+            }
+
+            return pixelArray;
+        }
+
+        private IEnumerable<Pixel> MakePixelsFrom32bppArgb()
+        {
+            var bps = new byte[Length];
+            System.Runtime.InteropServices.Marshal.Copy(data.Scan0, bps, 0, bps.Length);
+
+            byte selector = 0;
+            Pixel tmpPixel = new Pixel();
+            foreach (var v in bps)
+            {
+                switch (selector)
+                {
+                    case 0:
+                        tmpPixel.B = v;
+                        break;
+                    case 1:
+                        tmpPixel.G = v;
+                        break;
+                    case 2:
+                        tmpPixel.R = v;
+                        break;
+                    case 3:
+                        tmpPixel.A = v;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException("Pixelmap::MakePixels()::selector Error");
+                }
+
+                selector = (byte)(++selector % 4);
+
+                if (selector == 0)
+                {
+                    yield return tmpPixel;
+                    tmpPixel = new Pixel();
+                }
+            }
+        }
+
+        private IEnumerable<Pixel> MakePixelsFrom8bppIndexed()
+        {
+            var bps = new byte[Length];
+            System.Runtime.InteropServices.Marshal.Copy(data.Scan0, bps, 0, bps.Length);
+            ColorPalette palette = bitmap.Palette;
+
+            foreach (var v in bps)
+            {
+                yield return new Pixel(palette.Entries[v]);
+            }
+        }
+
+        #region Interface
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            data.PixelFormat = PixelFormat;
+            if (disposing)
+            {
+                System.Runtime.InteropServices.Marshal.Copy(pixels.SelectMany(p => p.ToBytes()).ToArray(), 0, data.Scan0, pixels.Length * 4);
+            }
+            bitmap.UnlockBits(data);
+        }
+
+        public IEnumerator<Pixel> GetEnumerator()
+        {
+            return ((IEnumerable<Pixel>)pixels).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return pixels.GetEnumerator();
+        }
+        #endregion
+
+        /// <summary>
+        /// Marshal.Copy()はしない
+        /// UnlockBits()はする
+        /// </summary>
+        ~Pixelmap()
+        {
+            Dispose(false);
+        }
+    }
+}
